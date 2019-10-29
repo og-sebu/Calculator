631a996b65f64c6b33c96b94d943d522857b14e4
og-sebu <54179684+og-sebu@users.noreply.github.com>
2019-10-29 15:20:43 +0100

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,26 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public static int getNumberFromInput(Scanner scanInput, String request) {
+    public static void main(String[] args) {
+
+        Scanner scanInput = new Scanner(System.in);
+
+        double num1 = getNumberFromInput(scanInput, "Enter first number (or enter 'c' to skip)");
+
+        double num2 = getNumberFromInput(scanInput, "Enter second number (or enter  'c' to skip)");
+
+        char operator = getOperatorFromInput(scanInput, "Which operator (+ , - , / , *) ");
+        double num3 = calculation(operator, num1, num2);
+
+        System.out.println(num3);
+
+    }
+
+    public static double getNumberFromInput(Scanner scanInput, String request) {
 
         System.out.println(request);
 
-        while (!scanInput.hasNextDouble()) {             //Prüft ob Eingabe Int/Double entspricht, wiederholt Anfrage falls nicht
+        while (!scanInput.hasNextDouble()) {
 
             if (scanInput.hasNext("c")) {
                 break;
@@ -17,11 +32,11 @@ public class Calculator {
 
         }
 
-        return scanInput.nextInt();
+        return scanInput.nextDouble();
 
     }
 
-    public static double calculation(char operator, int num1, int num2) {
+    public static double calculation(char operator, double num1, double num2) {
 
         double num3 = 0;
 
@@ -34,7 +49,7 @@ public class Calculator {
                 num3 = (num1 - num2);
                 break;
             case '/':
-                num3 = ((double) num1 / num2);
+                num3 = (num1 / num2);
                 break;
             case '*':
                 num3 = (num1 * num2);
@@ -50,25 +65,30 @@ public class Calculator {
 
         System.out.println(request);
 
+        while (scanInput.hasNext()) {               //Führt Schleife ständig aus bis Signalwort eingegeben wird
+
+            if (scanInput.hasNext("\\+" )) {                 //Break durch gegebenes Zeichen
+                break;
+            }
+
+            if (scanInput.hasNext("-")) {                 //Break durch gegebenes Zeichen
+                break;
+            }
+
+            if (scanInput.hasNext("/")) {                 //Break durch gegebenes Zeichen
+                break;
+            }
+
+            if (scanInput.hasNext("\\*")) {                 //Break durch gegebenes Zeichen
+                break;
+            }
+
+            System.out.println("Re-enter a operator (+ , - , / , *) ");
+            scanInput.next();
+
+        }
+
         return scanInput.next().charAt(0);
-
-    }
-
-
-    public static void main(String[] args) {
-
-        Scanner scanInput = new Scanner(System.in);
-
-        String request = "Enter first number (or enter 'c' to skip)";
-        int num1 = getNumberFromInput(scanInput, request);
-
-        request = "Enter second number (or enter  'c' to skip)";
-        int num2 = getNumberFromInput(scanInput, request);
-
-        char operator = getOperatorFromInput(scanInput, "Which operator (+ , - , / , *) ");
-        double num3 = calculation(operator, num1, num2);
-
-        System.out.println(num3);
 
     }
 
